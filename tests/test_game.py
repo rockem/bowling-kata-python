b5f4393fd2f8681bab4627ef2c13ef144624e5e9
Eli Segal <eli.segal@cs-robotics.com>
2020-01-23 12:24:48 +0200

    Refactor after all ones test passed

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,13 +1,22 @@
+import pytest
+
 from bowling.game import BowlingGame
 
 
-def test_gutter_game():
-    game = BowlingGame()
-    [game.roll(0) for _ in range(20)]
+@pytest.fixture()
+def game():
+    yield BowlingGame()
+
+
+def test_gutter_game(game):
+    _roll_many(game, pins=0, times=20)
     assert game.score() == 0
 
 
-def test_all_ones_game():
-    game = BowlingGame()
-    [game.roll(1) for _ in range(20)]
+def _roll_many(game, pins, times):
+    [game.roll(pins) for _ in range(times)]
+
+
+def test_all_ones_game(game):
+    _roll_many(game, pins=1, times=20)
     assert game.score() == 20
