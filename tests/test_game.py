7cbecea88f21a99cd5c02f4155a4bb0b8a7641c1
Eli Segal <eli.segal@cs-robotics.com>
2020-01-23 12:16:41 +0200

    Implemented all ones test

--- tests/test_game.py
+++ tests/test_game.py
@@ -5,3 +5,9 @@ def test_gutter_game():
     game = BowlingGame()
     [game.roll(0) for _ in range(20)]
     assert game.score() == 0
+
+
+def test_all_ones_game():
+    game = BowlingGame()
+    [game.roll(1) for _ in range(20)]
+    assert game.score() == 20
